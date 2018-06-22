c38ebe8ee94e90a1d51c5c050732d78054e4f6f0
Stuart Roskelley <runfaj@gmail.com>
2018-06-22 14:23:53 -0600

    testing

--- js/settings.js
+++ js/settings.js
@@ -49,15 +49,32 @@ function setSaveDisabledState() {
     var btn = jQuery('.project-save');
     btn.prop('disabled', !checkProjectValid());
 }
+function updateUserList() {
+    var select = jQuery('.users');
+    select.empty();
+    select.append("<option value='-1'></option>");
+
+    userList.sort(function(a,b){
+        return a.fullName - b.fullName;
+    });
+
+    for(var i=0;i<userList.length;i++) {
+        var fullName = userList[i].fullName;
+        var group = userList[i].group || 'none';
+        select.append("<option value='"+i+"'>"+fullName+" ("+group+")</option>");
+    }
+
+    console.log('user list:',userList);
+}
 function updateProjectList() {
     var select = jQuery('.projects');
+    select.empty();
+    select.append("<option value='-1'></option>");
 
     projectList.sort(function(a,b){
         return a.id - b.id;
     });
 
-    select.empty();
-    select.append("<option value='-1'></option>");
     for(var i=0;i<projectList.length;i++) {
         var projectName = projectList[i].name;
         var billingCode = projectList[i].code;
@@ -168,21 +185,52 @@ function onProjectChange() {
 function onRender() {
     return Promise.all([
             t.get('organization', 'shared', 'projects'),
+            t.get('organization', 'shared', 'memberGroups'),
             t.get('member', 'private', 'user_token'),
             t.organization('id')
         ])
-        .spread(function(projects, user_token, organization) {
+        .spread(function(projects, memberGroups, user_token, organization) {
             if(projects) projectList = projects;
             updateProjectList();
 
+            if(memberGroups) userList = memberGroups;
+
+            // get all the users of the organization and update member groups as needed
             Trello.get(
                 'organizations/' + organization.id + '/members',
                 {
                     key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
                     token: user_token
                 },
-                function success() {
-                    console.log(arguments)
+                function success(data, status, xhr) {
+                    //add all new members
+                    for(var i=0;i<data.length;i++) {
+                        var memberExists = -1;
+                        for(var j=0;j<memberGroups.length;j++) {
+                            if(data[i].id == memberGroups[j].id) {
+                                memberExists = j;
+                                break;
+                            }
+                        }
+                        if(memberExists == -1) {
+                            memberGroups.push(data[i]);
+                        }
+                    }
+                    //remove all stale members
+                    for(var j=memberGroups.length - 1;j>=0;j--) {
+                        var memberExists = -1;
+                        for(var i=0;i<data.length;i++) {
+                            if(data[i].id == memberGroups[j].id) {
+                                memberExists = j;
+                                break;
+                            }
+                        }
+                        if(memberExists == -1) {
+                            memberGroups.splice(j, 1);
+                        }
+                    }
+
+                    updateUserList();
                 },
                 function error() {}
             );
