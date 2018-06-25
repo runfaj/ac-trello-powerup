faba3f2e6f568ccaffbd3d7eadee221bd8d6c577
James Hulse <jamesh@allencomm.com>
2018-06-25 14:27:17 -0600

    test

--- js/task_view.js
+++ js/task_view.js
@@ -1,7 +1,16 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
+let boards = null;
 this.getBoards();
+
+console.log("boards", boards);
+
+for(var i = 0; i < boards.length; i++)
+{
+	this.getLists(boards[i]);
+	console.log('list ' + i);
+}
 
 function getBoards() {
     return Promise.all([
@@ -11,6 +20,29 @@ function getBoards() {
         .spread(function(user_token, organization) {
             Trello.get(
                 'organizations/' + organization.id + '/boards',
+                {
+                    key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
+                    token: user_token,
+                    filter: 'open',
+                    fields: 'name,id'
+                },
+                function success(data, responseText, xhr) {
+                    console.log('json data:', data)
+                    boards = data;
+                },
+                function error() {}
+            );
+        });
+}
+
+function getLists(board){
+	return Promise.all([
+            t.get('member', 'private', 'user_token'),
+            t.organization('id')
+        ])
+        .spread(function(user_token, organization) {
+            Trello.get(
+                'organizations/' + organization.id + '/boards' + board.id + '/lists',
                 {
                     key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
                     token: user_token,
