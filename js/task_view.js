92673ceb7a7a1f95c0d6745653f931b6fa2361d3
James Hulse <jamesh@allencomm.com>
2018-06-25 10:58:12 -0600

    test

--- js/task_view.js
+++ js/task_view.js
@@ -1,24 +1,6 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-var data = null;
-
-var xhr = new XMLHttpRequest();
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("GET", t.organization('id'));
-
-xhr.send(data);
-
-console.log('scope:', t.arg('scope'));
-
-console.log('Data', data);
-
 this.getBoards();
 
 function getBoards() {
