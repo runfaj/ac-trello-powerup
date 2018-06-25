2e8da4df35bd613a571e81e2c93e2d450fe5f0f7
Stuart Roskelley <runfaj@gmail.com>
2018-06-25 10:49:02 -0600

    testing

---
+++ js/hour_view.js
@@ -0,0 +1,23 @@
+function getBoards() {
+    return Promise.all(
+            t.get('member', 'private', 'user_token')
+            t.organization('id')
+        )
+        .spread(function(user_token, organizations) {
+            Trello.get(
+                'organizations/' + organizations[0].id + '/boards',
+                {
+                    key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
+                    token: user_token,
+                    filter: 'open',
+                    fields: 'name,id'
+                },
+                function success(data, responseText, xhr) {
+                    console.log('json data:', data)
+                },
+                function error() {}
+            );
+    });
+}
+
+getBoards();
