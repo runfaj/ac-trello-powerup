becd5b2b1939efd2ebb97ace467e76d610fb6fd5
James Hulse <jamesh@allencomm.com>
2018-06-25 10:53:47 -0600

    test

--- js/task_view.js
+++ js/task_view.js
@@ -11,7 +11,7 @@ xhr.addEventListener("readystatechange", function () {
   }
 });
 
-xhr.open("GET", "https://api.trello.com/1/organizations/allencommcw");
+xhr.open("GET", t.organization('id'));
 
 xhr.send(data);
 
@@ -19,5 +19,26 @@ console.log('scope:', t.arg('scope'));
 
 console.log('Data', data);
 
+this.getBoards();
 
-
+function getBoards() {
+    return Promise.all(
+            t.get('member', 'private', 'user_token')
+            t.organization('id')
+        )
+        .spread(function(user_token, organizations) {
+            Trello.get(
+                'organizations/' + organizations[0].id + '/boards',
+                {
+                    key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
+                    token: user_token,
+                    filter: 'open',
+                    fields: 'name,id'
+                },
+                function success(data, responseText, xhr) {
+                    console.log('json data:', data)
+                },
+                function error() {}
+            );
+    });
+}
