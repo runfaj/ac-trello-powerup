91974e8c5ed721d02ecc74b6fe485ae7bfcf7e67
Stuart Roskelley <runfaj@gmail.com>
2018-06-25 13:09:54 -0600

    testing

--- js/hour_view.js
+++ js/hour_view.js
@@ -1,26 +1,83 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-function getBoards() {
+var token, organizationId;
+var scope = t.arg('scope');
+var boards = [];
+var boardLists = [];
+
+function initialize(callback) {
     return Promise.all([
             t.get('member', 'private', 'user_token'),
             t.organization('id')
         ])
         .spread(function(user_token, organization) {
-            Trello.get(
-                'organizations/' + organization.id + '/boards',
-                {
-                    key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
-                    token: user_token,
-                    filter: 'open',
-                    fields: 'name,id'
-                },
-                function success(data, responseText, xhr) {
-                    console.log('json data:', data)
-                },
-                function error() {}
-            );
+            token = user_token;
+            organizationId = organization.id;
+
+            if(callback) callback();
         });
 }
 
-getBoards();
+function getBoards(callback) {
+    Trello.get(
+        'organizations/' + organizationId + '/boards',
+        {
+            key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
+            token: token,
+            filter: 'open',
+            fields: 'name,id'
+        },
+        function success(data, responseText, xhr) {
+            if(callback) callback(data);
+        },
+        function error() {}
+    );
+}
+
+function getListsInBoard(boardId, openOnly, includeArchived) {
+    Trello.get(
+        'boards/' + boardId + '/lists',
+        {
+            key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
+            token: token,
+            filter: includeArchived ? 'all' : 'open,none',
+            cards: includeArchived ? 'all' : 'open,none,visible',
+            card_fields: 'all',
+            fields: 'all'
+        },
+        function success(data, responseText, xhr) {
+            if(callback) callback(data);
+        },
+        function error() {}
+    );
+}
+
+
+initialize(function(){
+    var doneLoading = true;
+
+    if(scope != 'board') {
+        getBoards(function(data){
+            boards = data;
+
+            function waitForAllLists(timeLeft) {
+                if(boardLists.length === boards.length)
+                    doneLoading = true;
+                else
+                    setTimeout(waitForAllLists.bind(null, timeLeft-100), 100);
+            }
+
+            for(var i=0;i<boards.length;i++) {
+                getListsInBoard(function(data){
+                    boardLists[i] = data;
+                });
+            }
+
+            waitForAllLists(10000);
+        });
+    }
+
+    console.log(boardLists);
+
+});
