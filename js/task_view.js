94de461275fc74a93449a0490948ec438f880eb3
James Hulse <jamesh@allencomm.com>
2018-06-25 14:39:13 -0600

    test

--- js/task_view.js
+++ js/task_view.js
@@ -6,7 +6,27 @@ this.getBoards();
 
 console.log("boards", this.boards);
 
-
+function getLists(board){
+	return Promise.all([
+            t.get('member', 'private', 'user_token'),
+            t.organization('id')
+        ])
+        .spread(function(user_token, organization) {
+            Trello.get(
+                'organizations/' + organization.id + '/boards' + board.id + '/lists',
+                {
+                    key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
+                    token: user_token,
+                    filter: 'open',
+                    fields: 'name,id'
+                },
+                function success(data, responseText, xhr) {
+                    console.log('json data:', data)
+                },
+                function error() {}
+            );
+        });
+}
 
 function getBoards() {
     return Promise.all([
@@ -36,24 +56,3 @@ function getBoards() {
         });
 }
 
-function getLists(board){
-	return Promise.all([
-            t.get('member', 'private', 'user_token'),
-            t.organization('id')
-        ])
-        .spread(function(user_token, organization) {
-            Trello.get(
-                'organizations/' + organization.id + '/boards' + board.id + '/lists',
-                {
-                    key: 'ad42f1ee6ea8f3fe9e31018b5f861536',
-                    token: user_token,
-                    filter: 'open',
-                    fields: 'name,id'
-                },
-                function success(data, responseText, xhr) {
-                    console.log('json data:', data)
-                },
-                function error() {}
-            );
-        });
-}
